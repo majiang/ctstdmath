789e46165299b851b9691d73088b258b7be48e5c
majiang <neuron.ohori@gmail.com>
2016-11-19 16:52:48 +0900

    Add isInfinity

--- source/ctstdmath/package.d
+++ source/ctstdmath/package.d
@@ -1,1 +1,24 @@
 module ctstdmath;
+
+unittest
+{
+    import std.math;
+    enum a = real(1).abs, b = real(1).fabs, c = real(1).sqrt;
+}
+
+///
+bool isInfinity(real x)
+{
+    return x < -real.max || real.max < x;
+}
+///
+unittest
+{
+    static assert(!isInfinity(real.init));
+    static assert(!isInfinity(-real.init));
+    static assert(!isInfinity(real.nan));
+    static assert(!isInfinity(-real.nan));
+    static assert(isInfinity(real.infinity));
+    static assert(isInfinity(-real.infinity));
+    static assert(isInfinity(-1.0L / 0.0L));
+}
