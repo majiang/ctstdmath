331e8b6ceff3e099beb9e9be077460cebae4587d
majiang <neuron.ohori@gmail.com>
2016-11-19 17:20:45 +0900

    Add isNaN

--- source/ctstdmath/package.d
+++ source/ctstdmath/package.d
@@ -21,4 +21,21 @@ unittest
     static assert(isInfinity(real.infinity));
     static assert(isInfinity(-real.infinity));
     static assert(isInfinity(-1.0L / 0.0L));
+    static assert(!isInfinity(real(0)));
+}
+
+///
+bool isNaN(real x)
+{
+    return !(0 <= x) && !(x <= 0);
+}
+///
+unittest
+{
+    static assert(isNaN(real.nan));
+    static assert(isNaN(-real.nan));
+    static assert(!isNaN(real.infinity));
+    static assert(!isNaN(-real.infinity));
+    static assert(!isNaN(-1.0L / 0.0L));
+    static assert(!isNaN(real(0)));
 }
